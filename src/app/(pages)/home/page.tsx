d6252ee1cb913e50e136f481f1e1cbfad1d1683b
PetroYurchuk <petroyurchuk66@gmail.com>
2024-03-08 19:43:48 +0200

    authOptions exported to another file

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,4 +1,4 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/auth";
 import { UserInfo } from "@/components";
 import { getServerSession } from "next-auth";
 
